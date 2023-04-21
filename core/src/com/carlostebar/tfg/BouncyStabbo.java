53c4a2218f52e7a0cf8305ecfbb2e8fc34c69dc2
CarlosTheCrabbo <carloscinicshady@gmail.com>
2023-04-21 15:20:18 +0200

    feat: ahora hay obstaculos

--- core/src/com/carlostebar/tfg/BouncyStabbo.java
+++ core/src/com/carlostebar/tfg/BouncyStabbo.java
@@ -7,21 +7,40 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class BouncyStabbo extends ApplicationAdapter {
 	//Instancio una variable que con la que manejar el estado de la ejecucion
-	int estadoEjecucion = 1;
+	int estadoEjecucion = 0;
 	//Instancio el SpriteBatch que contendra las animaciones del juego
 	SpriteBatch miBatch;
 	//Instancio la textura del fondo y un array de texturas para el personaje y su movimiento
+	//Tambien instancio las texturas del fondo y de los obsrtaculos
+	Texture obstaculoArriba;
+	Texture obstaculoAbajo;
 	Texture fondo;
 	Texture[] personajePrincipal;
 	//Hago variables con las que controlar las coordenadas del personaje y su velocidad
 	double movimiento = 0;
 	double velocidadCaida=0.65;
-	long personajeCoordX = 0;
 	long personajeCoordY = 0;
+	//Hago una variable para definir el espacio entre obstaculos
+	float espadiadObstaculos = 100;
 
 
 	//hago una marca de control de la dfase del personaje principal
 	int controlPersonje = 0;
+
+	//Hago un metodo que recorra en bucle el estado del personaje para usarlo en el metodo render
+	public void recorreEstados(){
+		if(controlPersonje == 0){
+			controlPersonje = 1;
+		}else if(controlPersonje == 1){
+			controlPersonje = 2;
+		}else if (controlPersonje == 2){
+			controlPersonje = 3;
+		}else if (controlPersonje == 3){
+			controlPersonje = 4;
+		}else {
+			controlPersonje = 0;
+		}
+	}
 
 	//metodo on create que se arranca al iniciar
 	@Override
@@ -40,16 +59,30 @@ public class BouncyStabbo extends ApplicationAdapter {
 			personajePrincipal[4] = new Texture("ninja_salto5.png");
 
 		personajeCoordY =  Gdx.graphics.getHeight()/2- personajePrincipal[controlPersonje].getHeight()/2;
+
+		//Instancio las texturas de los obstaculos
+		obstaculoArriba = new Texture("obstaculo1.png");
+		obstaculoAbajo = new Texture("obstaculo2.png");
 	}
 
 	//renderizo las texturas cargadas en el metodo create
 	@Override
 	public void render () {
+
 		//Con este bucle registro si el usuario toca la pantalla
 		if(Gdx.input.justTouched()){
 			estadoEjecucion = 1;
 			movimiento= -30;
 		}
+
+		//Con este bucle hago que si el personaje llega a la parte de abajo de la pantalla su
+		// movimiento hacia abajo  se detiene de manera que parece que el suelo ha parado la
+		// caida del personaje
+		if(personajeCoordY > 0 || movimiento < 0){
+			movimiento = movimiento + velocidadCaida;
+			personajeCoordY -= movimiento;
+		}
+
 		//Con esto simulo la gravedad, de manera que la coordenada Y del personaje decrece
 		// exponencialmente, dando la sensación de que está cayendo
 
@@ -63,6 +96,10 @@ public class BouncyStabbo extends ApplicationAdapter {
 			miBatch.begin();
 			//Pinto el fondo
 			miBatch.draw(fondo, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+			miBatch.draw(obstaculoAbajo, Gdx.graphics.getWidth()  - obstaculoAbajo.getWidth(), Gdx.graphics.getHeight() / 2+ espadiadObstaculos);
+			miBatch.draw(obstaculoArriba, Gdx.graphics.getWidth()  - obstaculoArriba.getWidth(), Gdx.graphics.getHeight() / 2- espadiadObstaculos/2-obstaculoAbajo.getHeight());
+
 			//Pinto el personaje en todas sus fases
 			miBatch.draw(personajePrincipal[controlPersonje], Gdx.graphics.getWidth() / 2, personajeCoordY);
 			miBatch.end();
@@ -73,24 +110,4 @@ public class BouncyStabbo extends ApplicationAdapter {
 		}
 	}
 
-	@Override
-	public void dispose () {
-		miBatch.dispose();
-		fondo.dispose();
-	}
-
-	//Hago un metodo que recorra en bucle el estado del personaje para usarlo en el metodo render
-	public void recorreEstados(){
-		if(controlPersonje == 0){
-			controlPersonje = 1;
-		}else if(controlPersonje == 1){
-			controlPersonje = 2;
-		}else if (controlPersonje == 2){
-			controlPersonje = 3;
-		}else if (controlPersonje == 3){
-			controlPersonje = 4;
-		}else {
-			controlPersonje = 0;
-		}
-	}
 }
