ad5793e7d8e319ee51447288d94e6987ff1df71f
CarlosTheCrabbo <carloscinicshady@gmail.com>
2023-05-08 21:49:54 +0200

    feat: menu principal y botones redondos personalizados

--- android/src/com/carlostebar/tfg/MenuPrincipal.java
+++ android/src/com/carlostebar/tfg/MenuPrincipal.java
@@ -1,8 +1,28 @@
 package com.carlostebar.tfg;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuPrincipal extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_principal);
 
+        Button botonInsertar = findViewById(R.id.botonJugar);
+        botonInsertar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuPrincipal.this, AndroidLauncher.class);
+                startActivity(intent);
+            }
+
+
+        });
+    }
 }
